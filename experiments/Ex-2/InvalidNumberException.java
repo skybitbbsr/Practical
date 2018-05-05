6d0d4d04dc792b34a0bc5fc9ba819df83e2c3430
suryak9937 <suryakantarath02@gmail.com>
2018-05-05 21:59:25 +0530

    change in custom exception

--- experiments/Ex-2/InvalidNumberException.java
+++ experiments/Ex-2/InvalidNumberException.java
@@ -1,10 +1,1 @@
-public class InvalidNumberException extends Exception {
-    private double number;
-    public InvalidNumberException(double number) {
-        super("Number should be in between 10 to 40.\nInvalid Number: "+number);
-        this.number=number;
-    }
-    public double getNumber() {
-        return number;
-    }
-}
+public class InvalidNumberException extends Exception {    public InvalidNumberException(double number) {        super(String.valueOf(number));    }}
--- experiments/Ex-2/NumberApp.java
+++ experiments/Ex-2/NumberApp.java
@@ -1,21 +1,1 @@
-import java.util.Scanner;
-
-public class NumberApp {
-    public static void main(String[] args) {
-        Ope op = new Ope();
-        double sum=0;
-        Scanner sc=new Scanner(System.in);
-        double number=1;
-        System.out.println("Enter Numbers between 10 to 40 to be added...");
-        while(number!=0) {
-            try {
-                number=sc.nextDouble();
-                sum=op.setNumber(number);
-
-            }catch (InvalidNumberException e){
-                System.out.println(e.getMessage());
-            }
-        }
-        System.out.println("Sum= "+sum);
-    }
-}
+import java.util.Scanner;public class NumberApp {    public static void main(String[] args) /* throws InvalidNumberException (to be used when we don't use try and catch block.) */ {        Ope op = new Ope();        double sum=0;        Scanner sc=new Scanner(System.in);        double number=1;        System.out.println("Enter Numbers between 10 to 40 to be added...");        while(number>0) {            try {                number=sc.nextDouble();                sum=op.setNumber(number);            }catch (InvalidNumberException e){                System.out.println("Number should be in between 10 to 40.\nInvalid Number: "+e.getMessage());            }        }        System.out.println("Sum= "+sum);    }}
